824996d48e1576e6d4d62a5220bf63edf5ee8601
Yanira Morales <ylm4@njit.edu>
2016-11-17 18:54:56 -0500

    upload file

---
+++ upload.php
@@ -0,0 +1,29 @@
+<?php
+
+include_once "fileupload.php";
+
+define ("UPLOAD_DIR","/srv/www/uploads/");
+
+if (!empty($_FILES["myFile"])) {
+	$myFile=$_FILES["myFile"];
+
+	if($myFile["error"] !== UPLOAD_ERR_OK){
+		echo "<p>An error occurred.</p>";
+		exit;
+	}
+	$name = preg_replace("/[^A-Z0-9._-]/i", "_", $myFile["name"]);
+	$i-0
+	$parts = pathinfo($name);
+	while (file_exists(UPLOAD_DIR . $name)) {
+		$i++;
+		$name = $parts["filename" . "-" . $i . "." . $parts["extension"];
+	}
+	$success= move_uploaded_file($myFile["tmp_name"],
+		UPLOAD_DIR . $name);
+	if(!$success) {
+		echo "<p> Unable to save file. </p>";
+		exit;
+	}
+	chmod(UPLOAD_DIR . $name, 0644);
+}
+?>
